4be3bf6bc08b3f63b5f38af2b8d0edac2ff0e845
Paul Sexton <github@psexton.net>
2021-05-09 12:57:27 -0500

    Python app stub

--- worth.py
+++ worth.py
@@ -1,16 +1,27 @@
-# look up current price
-# convert RSUs into date/value pairs
-# convert options into date/value pairs
-# compute total value
+#!/usr/bin/env python
 
-# split out unvested equity from vested
 
-# sort unvested values by date
-# for each date, subtract amount that vests on that date
-# for each threshold, check if remaining amount is less
-# iterate until we run out of thresholds or we run out of equity
-# produce threshold/date pairs
+def main():
+    # read in config file
 
-# for each threshold date, compute duration from now
+    # look up current price
+    # convert RSUs into date/value pairs
+    # convert options into date/value pairs
+    # compute total value
 
-# pretty print
+    # split out unvested equity from vested
+
+    # sort unvested values by date
+    # for each date, subtract amount that vests on that date
+    # for each threshold, check if remaining amount is less
+    # iterate until we run out of thresholds or we run out of equity
+    # produce threshold/date pairs
+
+    # for each threshold date, compute duration from now
+
+    # pretty print
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
