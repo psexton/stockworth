3faa226a5d7ba2038efa1b32031c72121aba154a
Paul Sexton <github@psexton.net>
2021-05-11 21:56:13 -0500

    Add equity class

---
+++ equity.py
@@ -0,0 +1,16 @@
+class Equity:
+    def __init__(self, date, value):
+        self.date = date
+        self.value = value
+
+    @staticmethod
+    def from_rsu(current_price, quantity, vest_date):
+        value = current_price * quantity
+        return Equity(vest_date, value)
+
+    @staticmethod
+    def from_option(current_price, quantity, vest_date, strike_price):
+        purchase_price = quantity * strike_price
+        sale_price = current_price * quantity
+        value = sale_price - purchase_price
+        return Equity(vest_date, value)
