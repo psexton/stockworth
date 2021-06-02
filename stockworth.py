f10bb8d45c736804473ef3cc4863a772b940c7b7
Paul Sexton <github@psexton.net>
2021-06-01 19:00:00 -0500

    Add license preamble

--- stockworth.py
+++ stockworth.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+# <editor-fold desc="AGPLv3 preamble">
+# stockworth, a simple equity pretty printer
+# Copyright (C) 2021  Paul Sexton
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# </editor-fold>
+
 import argparse
 import json
 import os
